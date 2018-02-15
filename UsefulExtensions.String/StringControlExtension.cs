4e4d37ba5b216ad955982bde580eb0a91829be23
Hüseyin Ulas <huseyin.ulas@trendyol.com>
2018-02-15 15:53:03 +0300

    string toint conversion and isnumber control is added

---
+++ UsefulExtensions.String/StringControlExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace UsefulExtensions.String
+{
+    public static class StringControlExtension
+    {
+        public static bool IsNumeric(this string value)
+        {
+            long retNum;
+            return long.TryParse(value, NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out retNum);
+        }
+    }
+}
---
+++ UsefulExtensions.String/StringConversionExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsefulExtensions.String
+{
+    public static class StringConversionExtension
+    {
+        public static int ToInt(this string value)
+        {
+            return Convert.ToInt32(value);
+        }
+    }
+}
